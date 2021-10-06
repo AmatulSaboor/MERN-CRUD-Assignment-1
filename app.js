3aa39a0772e0fcca21a2a5f837c19e578ebd7f2f
Amatul Saboor Siddiqua <amatulsaboor221@gmail.com>
2021-10-06 08:10:44 +0500

    ESLINT applied

--- app.js
+++ app.js
@@ -1,66 +1,66 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 const port = 3000;
 
 app.use(express.urlencoded());
 app.use(express.static('public'));
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 
-// =================================== index page (read) ==================================
+// ======================= index page (read) ==================================
 app.get('/', (req, res) => {
-    fs.readFile(path.join(__dirname, 'data/user_info.json'), (err, data)=>
-    {
-        const userData = JSON.parse(data.toString());
-        if(err) throw err;
-        res.render('./index', {userData});
-    });
+  fs.readFile(path.join(__dirname, 'data/user_info.json'), (err, data)=> {
+    const userData = JSON.parse(data.toString());
+    if (err) throw err;
+    res.render('./index', {userData});
+  });
 });
 
-// ==================================== create =========================================
+// =========================== create =========================================
 app.post('/add', (req, res) =>{
-    fs.readFile(path.join(__dirname, 'data/user_info.json'), (err, data) =>{
-        if (err) throw err;
-        const userData = JSON.parse(data.toString());
-        userData.push(req.body);
-        fs.writeFile(path.join(__dirname, 'data/user_info.json'), JSON.stringify(userData, null, 4), err =>
-        {
-            if(err) throw err;
+  fs.readFile(path.join(__dirname, 'data/user_info.json'), (err, data) =>{
+    if (err) throw err;
+    const userData = JSON.parse(data.toString());
+    userData.push(req.body);
+    fs.writeFile(path.join(__dirname, 'data/user_info.json'),
+        JSON.stringify(userData, null, 4), (err) => {
+          if (err) throw err;
         });
-    });
-    res.redirect('/');
+  });
+  res.redirect('/');
 });
 
-// ==================================== delete ======================================
+// ============================== delete ======================================
 app.get('/delete/:userId', (req, res) =>{
-    fs.readFile(path.join(__dirname, 'data/user_info.json'), (err, data) =>{
-        if (err) throw err;
-        console.log(req.params.userId);
-        const userData = JSON.parse(data.toString());
-        userData.splice(userData.findIndex(x => x.id == req.params.userId), 1);
-        fs.writeFile(path.join(__dirname, 'data/user_info.json'), JSON.stringify(userData, null, 4), err =>
-        {
-            if(err) throw err;
+  fs.readFile(path.join(__dirname, 'data/user_info.json'), (err, data) =>{
+    if (err) throw err;
+    console.log(req.params.userId);
+    const userData = JSON.parse(data.toString());
+    userData.splice(userData.findIndex((x) => x.id == req.params.userId), 1);
+    fs.writeFile(path.join(__dirname, 'data/user_info.json'),
+        JSON.stringify(userData, null, 4), (err) => {
+          if (err) throw err;
         });
-    });
-    res.redirect('/');
+  });
+  res.redirect('/');
 });
 
-// ==================================== update ======================================
+// ============================== update ======================================
 app.post('/edit', (req, res) =>{
-    console.log(req.body);
-    fs.readFile(path.join(__dirname, 'data/user_info.json'), (err, data) =>{
-        if (err) throw err;
-        const userData = JSON.parse(data.toString());
-        userData.splice(userData.findIndex(x => x.id == req.body.id), 1, req.body);
-        fs.writeFile(path.join(__dirname, 'data/user_info.json'), JSON.stringify(userData, null, 4), err =>
-        {
-            if(err) throw err;
+  console.log(req.body);
+  fs.readFile(path.join(__dirname, 'data/user_info.json'), (err, data) =>{
+    if (err) throw err;
+    const userData = JSON.parse(data.toString());
+    userData.splice(userData.findIndex(
+        (x) => x.id == req.body.id), 1, req.body);
+    fs.writeFile(path.join(__dirname, 'data/user_info.json'),
+        JSON.stringify(userData, null, 4), (err) => {
+          if (err) throw err;
         });
-    });
-    res.redirect('/');
+  });
+  res.redirect('/');
 });
 
 app.listen(port);
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,7 +1,12 @@
-function openModalEditUser(user){
-    $("#idEdit").val(user.id);
-    $("#nameEdit").val(user.name);
-    $("#emailEdit").val(user.email);
-    $("#addressEdit").val(user.address);
-    $("#phoneEdit").val(user.phone);
+/**
+ * This is a function to open a modal
+ * and show all the details of the selected user
+ * @param {user} user
+ */
+function openModalEditUser(user) {
+  $('#idEdit').val(user.id);
+  $('#nameEdit').val(user.name);
+  $('#emailEdit').val(user.email);
+  $('#addressEdit').val(user.address);
+  $('#phoneEdit').val(user.phone);
 }
