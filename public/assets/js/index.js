09c7f43feddebf9fd7589e5dc23650c05edc9b73
Amatul Saboor Siddiqua <amatulsaboor221@gmail.com>
2021-10-10 00:46:50 +0500

    edit user bug fixed

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-unused-vars */
 /**
  * This is a function to open a modal
  * and show all the details of the selected user
  * @param {user} user
  */
 function openModalEditUser(user) {
+  console.log(user);
   $('#idEdit').val(user.id);
   $('#nameEdit').val(user.name);
   $('#emailEdit').val(user.email);
